371777de175f7ede291086c9ac6db9d0b98b557f
Femke van Esch <f.a.w.j.vanesch@uu.nl>
2023-12-08 16:29:44 +0100

    Added two new functions to create edge and nodelists from data: prepare_lists_from_edgelists.R and align_edge_nodelist.R. Moved old function to create lists to todo folder. Also corrected the name of the evaluation_step.R function

---
+++ R/align_edge_nodelist.R
@@ -0,0 +1,56 @@
+#' align_edge_nodelist
+#'
+#' This function is used to align the values of concepts in the edgelist and manually
+#' edited nodelist so that the value of the concepts in both lists align with 
+#' each other.You only need to run this function if you changed the default 
+#' value of any of the concepts in the nodelist after running the 
+#' 'prepare_lists_from_edgelist' function manually. You do not need to run it 
+#' when you only added information in the paradigm or instruments columns of 
+#' the nodelist
+#'  
+#' It takes the edgelist which was returns from the 'prepare_lists_from_edgelist'
+#' function and the nodelist return by the same function with changes in the value
+#' column. Categorizing concepts as paradigmatic or as instruments is optional, 
+#' but required to run the functions paradigm_support and instrument_support 
+#' functions - see more instruction in the documentation of these functions).
+#' 
+#' Run the following lines of code to save the edge and nodelist
+#' speaker_edgelist <- align_edge_nodelist (edgelist, nodelist)[[1]]
+#' speaker_nodelist <- align_edge_nodelist (edgelist, nodelist)[[2]]
+#' 
+#' You may store the nodelist as csv and fill in the value, paradigms and 
+#' instruments column via excel by running the following code:
+#' write.csv(df,file='/.../new_file.csv',fileEncoding = "UTF-8")
+#' 
+#'
+#' @param edgelist an edgelist
+#' @param edgelist an nodelist
+#' @return Returns an edgelist and nodelist
+#' @export
+#' @examples
+#' # INCOMPLETE
+#' # Load the data
+#' data("edgelist", "nodelist")
+#' 
+#' Run the align_edge_nodelist function
+#'
+align_edge_nodelist <- function(edgelist, nodelist){
+
+#drop the old value.x and value.y columns
+edgelist <- dplyr::select(edgelist, -c(value.x,value.y))
+
+#derive only the ids and values from the nodelist
+nodes <- dplyr:: select(nodelist, c(id,value))
+
+# join edgeslist and nodes df by id's 
+#first for the 'from' concepts
+edgelist <- dplyr::left_join(edgelist, nodes, by = c("from" = "id"))
+
+#then for the 'to' concepts
+edgelist <- dplyr::left_join(edgelist, nodes, by = c("to" = "id"))
+
+#beware it renames the first(from) id to id.x and the second(to)id to id.y
+
+#return the edgelist and a nodelist to which paradigms and instruments can be addedrut
+base::return (list(edgelist, nodelist))
+}
---
+++ R/prepare_lists_from_edgelist.R
@@ -0,0 +1,116 @@
+#' prepare_lists_from_edgelist
+#'
+#' This function is used to determine to derive a nodelist from a simple edgelist
+#' as well as reformat a simple edgelist so it will ready for analysis with the 
+#' cognitivemapr Rpackage. 
+#' It takes an edgelist with the following 4 mandatory columns:
+#' "from": cause concept
+#' "to": effect concept
+#' "weight": number of times the relation is mentioned (can have any number, 
+#'           if left empty the function will set it by default to 1)
+#' "edge_value": sign/value of the relation: can be 1 (positive relation), 
+#'            -1 (negative relation), or 0 (non-existent relation). If left empty
+#'            the function will set it by default to 1 (positive)
+#'  in addition any number of columns with meta-data may be added.
+#' 
+#' The function replaces the concepts in the edgelist with id's that
+#' correspond to those in the nodelist and reorders the columns, to match the 
+#' requirements of the other functions in this package. It checks whether columns 
+#' weight and edge-value in the edgelist have values, and sets these values to 
+#' the default of 1 (weight = 1, edge_value = positive)if they were left empty. 
+#' It assumes all the concepts in the edgelist are in principle normatively positive 
+#' or neutral by adding two columns: value_x (value of concepts in 'from' column) 
+#' and value_y (value of concepts in 'to'  column) and sets all values to 1 (=positive)
+#' 
+#' It derives a nodelist from the edgelist with all unique concepts an an id to 
+#' link the edge and nodelists. It adds three  columns to the nodelist, 
+#' a column 'value' which is set to 1 (concept has a positive or neutral value) 
+#' as default. This value may be changed manually in excel if needed after 
+#' running this function. It also adds two empty columns named "paradigms" and 
+#' "instruments", for the researcher to fill in manually via excel after running 
+#' the function. Categorizing concepts as paradigmatic or as instruments is optional, 
+#' but required to run the functions paradigm_support and instrument_support 
+#' functions - see more instruction in the documentation of these functions).
+#' 
+#' Run the following lines of code to save the edge and nodelist
+#' speaker_edgelist <- prepare_lists_from_edgelist (edgelist)[[1]]
+#' speaker_nodelist <- prepare_lists_from_edgelist (edgelist)[[2]]
+#' 
+#' You may store the nodelist as csv and fill in the value, paradigms and 
+#' instruments column via excel by running the following code:
+#' write.csv(df,file='/.../new_file.csv',fileEncoding = "UTF-8")
+#' 
+#'
+#' @param edgelist an edgelist
+#' @param nodelist a nodelist, if you want to run the paradigm_support and 
+#' instrument_support functions, you must fill in the designated columns in the
+#' nodelist after running this function
+#' @return Returns a list with the resulting edgelist and nodelist
+#' @export
+#' @examples
+#' # INCOMPLETE
+#' # Load the data
+#' data("edgelist")
+#' 
+#' Run the prepare_lists_from_edgelist function
+#'
+prepare_lists_from_edgelist <- function(edgelist){
+
+#list only unique concepts in from-column
+nodelist_from <- base::unique(edgelist$from)
+
+#list only the unique concepts in to-column
+nodelist_to <- base::unique(edgelist$to)
+
+#join the two nodelist together
+nodelist <- c(nodelist_from, nodelist_to)
+
+#retain only the unique values
+nodelist <- base::unique(nodelist)
+
+#make vector into a dataframe
+nodelist <- base::as.data.frame(nodelist)
+
+#rename the column into node_name
+nodelist <- dplyr::rename(nodelist, node_name = nodelist)
+
+#put id's in front of the nodes
+nodelist <- tibble::rowid_to_column(nodelist, "id")
+
+#add a value column to the nodelist, set all values to 1 (=positive)
+nodelist <- base::cbind(nodelist, value = 1)
+
+#add corresponding id's for the concepts from the nodelist to the edgelist 
+#first for the 'from' concepts
+edgelist <- dplyr::left_join(edgelist, nodelist, by = c("from" = "node_name"))
+
+#then for the 'to' concepts
+edgelist <- dplyr::left_join(edgelist, nodelist, by = c("to" = "node_name"))
+
+#beware it renames the first(from) id to id.x and the second(to)id to id.y
+
+#drop the concept names: so the from and to columns with the concept names
+edgelist <- dplyr::select(edgelist, -c(from,to))
+
+#rename id.x to 'from' 
+edgelist <- dplyr::rename(edgelist, from = id.x)
+
+#rename id.y to 'to'
+edgelist <- dplyr::rename(edgelist, to = id.y)
+
+#to put the from and to columns in the first two columns in edgelist (needed to 
+#turn it into a CM later) when we do not know what kind and how much meta-data 
+#is in the edgelist, use the dplyr select function with the option 'everything()' 
+edgelist <- dplyr::select(edgelist, from, to, weight, everything())
+
+#safety check: if weight and edge-value are left open, replace with a value 1,
+#so the map reads that all edges are included once with a positive sign
+edgelist <- dplyr::mutate(edgelist, weight = ifelse(is.na(weight), 1, weight))
+edgelist <- dplyr::mutate(edgelist, edge_value = ifelse(is.na(edge_value), 1, edge_value))
+
+#now return to the nodelist and add empty columns for optional node characteristics
+nodelist <- base::cbind(nodelist, paradigms = NA, instruments = NA)
+
+#return the edgelist and a nodelist to which paradigms and instruments can be addedrut
+base::return (list(edgelist, nodelist))
+}
